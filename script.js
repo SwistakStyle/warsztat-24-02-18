14cb2519468cb963e09aebc8f2982657e38209e3
Sebastian Przesmycki <sebastian.przesmycki@gmail.com>
2018-02-24 12:50:40 +0100

    Objects

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ console.log(numberVariable);
 console.log(boolVariable);
 console.log(nullVariable);
 console.log(undefinedVariable);
+
+var simplestObject = {
+    name: 'Seb',
+    lastname: 'P'
+}
+
+console.log(simplestObject);
+
+var nestedObj = {
+    name: 'Seb',
+    car: {
+        brand: 'Audi',
+        model: 'A3'
+    }
+}
+
+console.log(nestedObj.car.brand);
+var simpleArray = [1,
+    2,
+    3,
+    4,
+    5, 
+    {name: 'Seb'}];
+console.log(simpleArray[1])
