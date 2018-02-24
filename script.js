634c1033e4779d8d6ef7abd62f7bb819b8437650
Sebastian Przesmycki <sebastian.przesmycki@gmail.com>
2018-02-24 17:58:51 +0100

    cool game

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
-(function(){
+(function () {
     var points
     var time
     var gameIntervalId
 
-    function addPoint(){
+    function addPoint() {
         points++
-        console.log('Dodano punkt!', points)
         displayPoints(points)
     }
 
-    function displayPoints(pointsParam){
+    function reduceTime() {
+        time--
+        displayTime(time)
+        if (time === 0) {
+            endGame()
+        }
+    }
+
+    function displayPoints(pointsParam) {
         var pointsContainer = document.querySelector('.points')
         pointsContainer.innerText = "Score: " + pointsParam
     }
 
-    function displayTime(timeParam){
+    function displayTime(timeParam) {
         var pointsContainer = document.querySelector('.time')
         pointsContainer.innerText = "Time: " + timeParam
     }
 
-    function makeMole(){
+    function flashBackground() {
+        var body = document.querySelector('body')
+        body.style.backgroundColor = 'red'
+        setTimeout(
+            function () {
+                body.style.backgroundColor = 'green'
+            },
+            100
+        )
+    }
+
+    function makeMole() {
         var molePosX = Math.round(
             Math.random() * (window.innerWidth - window.innerHeight / 10)
         )
         var molePosY = Math.round(
             Math.random() * (window.innerHeight - window.innerHeight / 10)
         )
-        var mole = document.createElement('div')   
+        var mole = document.createElement('div')
         mole.classList.add('mole')
         mole.style.left = molePosX + 'px'
         mole.style.top = molePosY + 'px'
 
         mole.addEventListener(
             'click',
-            function(){
+            function () {
                 mole.remove()
                 addPoint()
+                flashBackground()
             }
         )
         document.querySelector('body').appendChild(mole)
@@ -43,27 +62,55 @@
         return mole
     }
 
-    function init(){
-        points = 0
-        time = 10
-        mole = makeMole()
-        displayPoints(points)
-        displayTime(time)
+    function endGame() {
+        clearInterval(gameIntervalId)
+        mole.remove()
 
-        gameIntervalId = setInterval(
-            function(){
-                mole.remove()
+        document.querySelector('.end-modal .score')
+            .innerText = points + ' punktów!'
+        document.querySelector('.end-modal')
+            .style.display = 'block'
 
-                displayTime(time--)
-                if (time == 0){
-                    clearInterval(gameIntervalId)
-                    window.alert('Time is gone, your score: ' + points)
-                }else{
-                    mole = makeMole()
+        document.querySelector('.end-modal')
+            .addEventListener(
+                'click',
+                function(){
+                    document.querySelector('.end-modal')
+                        .style.display = 'none'
+                    init()
+                    startGame()
                 }
+            )
+    }
+
+    function startGame() {
+        mole = makeMole()
+        gameIntervalId = setInterval(
+            function () {
+                mole.remove()
+                mole = makeMole()
+                reduceTime()
             },
             1000
         )
+    }
+
+    function init() {
+        points = 0
+        time = 3
+        mole = null
+        displayPoints(points)
+        displayTime(time)
+
+        document.querySelector('.start-modal button')
+            .addEventListener(
+                'click',
+                function () {
+                    document.querySelector('.start-modal')
+                        .style.display = 'none'
+                    startGame()
+                }
+            )
     }
 
     init()
