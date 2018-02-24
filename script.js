4e4f7471d6606daa233277ad91ca9700be7c88e8
Sebastian Przesmycki <sebastian.przesmycki@gmail.com>
2018-02-24 13:27:39 +0100

    interval

--- script.js
+++ script.js
@@ -1,37 +1,6 @@
-console.log('Hello Seb!')
-
-var textVariable = "Seba";
-var numberVariable = 55;
-var boolVariable = true;
-var nullVariable = null;
-var undefinedVariable = undefined;
-
-console.log(textVariable);
-console.log(numberVariable);
-console.log(boolVariable);
-console.log(nullVariable);
-console.log(undefinedVariable);
-
-var simplestObject = {
-    name: 'Seb',
-    lastname: 'P'
-}
-
-console.log(simplestObject);
-
-var nestedObj = {
-    name: 'Seb',
-    car: {
-        brand: 'Audi',
-        model: 'A3'
-    }
-}
-
-console.log(nestedObj.car.brand);
-var simpleArray = [1,
-    2,
-    3,
-    4,
-    5, 
-    {name: 'Seb'}];
-console.log(simpleArray[1])
+setInterval(
+    function (){
+        console.log('Minela sekunda')
+    },
+    1000
+)
