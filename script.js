c14123cb5aa36eef143dc108a3f5c433080ff03c
Sebastian Przesmycki <sebastian.przesmycki@gmail.com>
2018-02-24 12:28:39 +0100

    primitives

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('Hello Seb!')
+
+var textVariable = "Seba";
+var numberVariable = 55;
+var boolVariable = true;
+var nullVariable = null;
+var undefinedVariable = undefined;
+
+console.log(textVariable);
+console.log(numberVariable);
+console.log(boolVariable);
+console.log(nullVariable);
+console.log(undefinedVariable);
